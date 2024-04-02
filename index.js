120ead8a085f170ad455a06e47dd3d3e29a3d4fa
whitney shisia <shisiawhitney215@gmail.com>
2024-04-02 12:20:30 +0300

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+    fetch('https://anapioficeandfire.com/api/books')
+      .then(response => response.json())
+      .then(data => renderBooks(data))
+      .catch(error => console.error('Error fetching books:', error));
+  }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
